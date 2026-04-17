838ef703f55708e2cf74def0bcc11b1291c1740e
Sam-Rags <168376297+Sam-Rags@users.noreply.github.com>
2026-04-17 19:07:45 -0400

    cors install

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 // setup this is similar to when we use default tags in html
 const express = require("express");
+// have to use cors to host front end & back end on same device
+var cors = require('cors');
 // activate or tell this app variable to be an express server
 const app = express();
+app.use(cors());
 const router = express.Router();
 
+
 //start the web server
-app.listen(3000, function() {
-    console.log("Listening on port 3000")
-})
+
 /**making an api using routes
  routes are used to handle browser requests. They look like URLs. The difference is that when a browser requests a route, 
  it is dynamically handled by using a function
   */
 
-// GET or a regular request when someone goes to http://localhost:3000/hello, when using a fx in a route, almost always have a parameter or handle a response & request
+router.get("/songs", function(req, res){
+    const songs = [
+        {
+            title: "We Found Love",
+            artist: "Rhianna",
+            popularity: 10,
+            releaseDate: new Date(2011, 9, 22),
+            genre: ["electro house"]
+        },
+        {
+            title: "Happy",
+            artist: "Pharrell Williams",
+            popularity: 10,
+            releaseDate: new Date(2013, 11, 21),
+            genre: ["soul", "new soul"]
+        }
+    ];
+
+    res.json(songs);
+})
+
+// all requests that usually use an api start with /api e.g. localhost:3000/api/songs
+app.use("/api", router)
+app.listen(3000)
